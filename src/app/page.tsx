e679bbf75262876aa3f94989a38142626beb64f9
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-06-17 23:42:52 +0530

    chore: Update page.tsx to fetch and display users from the database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-import { CreatePost } from "~/app/_components/create-post";
-import { api } from "~/trpc/server";
 
 export default async function Home() {
-
+  const users = await db.user.findMany();;
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h1>Hello World</h1>
+      {users.map((user) => (
+        <Link key={user.id} href={`/user/${user.id}`}>
+          {user.id}
+        </Link>
+      ))}
     </main>
   );
 }
