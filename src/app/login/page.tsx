1f3997d75b7673814b87d75d1d3cb34bb32bbe89
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-06-20 14:50:03 +0530

    login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,2 +1,57 @@
-import * as z from 'zod';
+"use client";;
 import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useForm } from "react-hook-form";
+import { Button } from "~/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { TLoginSchema, loginSchema } from "~/server/api/schema/zod-schema";
+
+export default function Login() {
+  const form = useForm<TLoginSchema>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onLogin = (data: TLoginSchema) => {
+    console.log(data);
+  };
+  return (
+    <div className="flex border rounded-lg w-48 h-52">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onLogin)}>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
--- src/server/api/schema/zod-schema.ts
+++ src/server/api/schema/zod-schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-    email: z.string().email().regex(/^[^\s@]+@sjec\.ac\.in$/),
+    email: z.string().email().regex(/^[^\s@]+@sjec\.ac\.in$/, "must use sjec email id"),
     password: z.string(),
 });
 
