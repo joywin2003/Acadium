72fde7e98e427c451c6aa791ce9d14a6866114ba
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-06-22 22:27:07 +0530

    designed the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 import {LoginButton} from "@/components/login-button";
+import AuthenticationPage from "./components/AuthenticationPage";
 
 
 export default async function Home() {
-  const users = await db.user.findMany();;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <LoginButton>
-        <button>Login</button>
-      </LoginButton>
-    </main>
+    <AuthenticationPage />
   );
 }
 
