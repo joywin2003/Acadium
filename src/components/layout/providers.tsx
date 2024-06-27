d59f7bd88be5a59f4df0709e4b38cb5b45e0d698
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-06-27 13:24:10 +0530

    added query provider in the provider.tsx

--- src/components/layout/providers.tsx
+++ src/components/layout/providers.tsx
@@ -1,16 +1,17 @@
-'use client';
-import React from 'react';
-import ThemeProvider from './ThemeToggle/theme-provider';
-export default function Providers({
-  children
-}: {
-  children: React.ReactNode;
-}) {
+"use client";
+import React from "react";
+import ThemeProvider from "./ThemeToggle/theme-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
+export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        {children}
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
 }
