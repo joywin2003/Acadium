bb60f7a3a04ac3b9d59ee356418a5c7573df9b35
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-07-06 13:32:46 +0530

    feat:added faculty form

---
+++ src/components/forms/faculty-form.tsx
@@ -0,0 +1,154 @@
+'use client';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Trash } from 'lucide-react';
+import { useParams, useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+import * as z from 'zod';
+import { Button } from '~/components/ui/button';
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from '~/components/ui/form';
+import { Heading } from '~/components/ui/heading';
+import { Input } from '~/components/ui/input';
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue
+} from '~/components/ui/select';
+import { Separator } from '~/components/ui/separator';
+import { facultyFormSchema, type TFacultyFormSchema } from '~/server/api/schema/zod-schema';
+// import FileUpload from '../file-upload';
+// import { useToast } from '../ui/use-toast';
+
+
+
+
+
+const defaultValues: TFacultyFormSchema = {
+    name: '',
+    email: '',
+    phone: '',
+    branch: 'CSE',
+    subjects: [''],
+};
+
+
+export function FacultyForm() {
+
+    const form = useForm<TFacultyFormSchema>({
+        resolver: zodResolver(facultyFormSchema),
+        defaultValues
+    });
+
+    const onSubmit = async (data: TFacultyFormSchema) => {
+        // console.log(data);
+    }
+
+    return (
+        <>
+            <Heading title="Add New Faculty" description='' />
+            <Separator />
+
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="w-full space-y-8"
+                >
+                    <div className="gap-8 md:grid md:grid-cols-3">
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Name" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Email" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="phone"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Phone</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Phone" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="branch"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Branch</FormLabel>
+                                    <FormControl>
+                                        <Select onValueChange={field.onChange}>
+                                            <SelectTrigger>
+                                                <SelectValue placeholder="Select Branch" />
+                                            </SelectTrigger>
+                                            <SelectContent>
+                                                <SelectItem value="CSE">CSE</SelectItem>
+                                                <SelectItem value="IT">IT</SelectItem>
+                                                <SelectItem value="ECE">ECE</SelectItem>
+                                                <SelectItem value="EEE">EEE</SelectItem>
+                                                <SelectItem value="MECH">MECH</SelectItem>
+                                                <SelectItem value="CIVIL">CIVIL</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="subjects"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Subjects</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Subjects" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                    </div>
+                    <div className="flex items-center justify-end gap-2 p-8">
+                        <Button type="submit">Submit</Button>
+                    </div>
+                </form>
+            </Form>
+        </>
+
+    )
+}
