b10ccb0455b0cf35be3afe42f0ee461ac14dda42
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-07-06 16:25:08 +0530

    fix:fixed the duplicate usn issue

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,10 +1,14 @@
 "use server";
 
-
-import { Faculty, Student} from "~/types";
+import { Faculty, Student } from "~/types";
 import { db } from "~/server/db";
 import { User } from "~/types";
-import { TFacultyFormSchema, TStudentFormSchema, facultyFormSchema, studentFormSchema } from "~/server/api/schema/zod-schema";
+import {
+  TFacultyFormSchema,
+  TStudentFormSchema,
+  facultyFormSchema,
+  studentFormSchema,
+} from "~/server/api/schema/zod-schema";
 
 export const getStudentList = async () => {
   const students: Student[] = await db.student.findMany();
@@ -23,7 +27,7 @@ export const getUserProfile = async (email: string) => {
   console.log(user);
   const userTemp: User = {
     id: user?.id || "",
-    usn:  "",
+    usn: "",
     role: user?.role || "student",
     name: "John Doe",
     email: user?.email || "",
@@ -31,26 +35,38 @@ export const getUserProfile = async (email: string) => {
   };
 
   return userTemp;
-}
+};
 
 export const createStudent = async (student: TStudentFormSchema) => {
   const validationResult = studentFormSchema.safeParse(student);
 
   if (!validationResult.success) {
-    throw new Error("Validation error: " + validationResult.error.errors.map(e => e.message).join(", "));
+    throw new Error(
+      "Validation error: " +
+        validationResult.error.errors.map((e) => e.message).join(", "),
+    );
+  }
+
+  const existingUSN = await db.student.findUnique({
+    where: { usn: student.usn },
+  });
+  if (existingUSN) {
+    throw new Error("A student with this USN already exists");
   }
 
   const existingUser = await db.student.findMany({
-      where: {
-          OR:[
-              {email: student.email},
-              {phone: student.phone}
-          ]
-      }
+    where: {
+      OR: [
+        { email: student.email },
+        { phone: student.phone },
+      ],
+    },
   });
 
   if (existingUser.length > 0) {
-    throw new Error("A student with this email or phone number already exists");
+    throw new Error(
+      "A student with this email or phone number already exists",
+    );
   }
 
   const newStudent = await db.student.create({
@@ -63,20 +79,22 @@ export const createFaculty = async (faculty: TFacultyFormSchema) => {
   const validationResult = facultyFormSchema.safeParse(faculty);
 
   if (!validationResult.success) {
-    throw new Error("Validation error: " + validationResult.error.errors.map(e => e.message).join(", "));
+    throw new Error(
+      "Validation error: " +
+        validationResult.error.errors.map((e) => e.message).join(", "),
+    );
   }
 
   const existingUser = await db.faculty.findMany({
-      where: {
-          OR:[
-              {email: faculty.email},
-              {phone: faculty.phone}
-          ]
-      }
-  }); 
+    where: {
+      OR: [{ email: faculty.email }, { phone: faculty.phone }],
+    },
+  });
 
   if (existingUser.length > 0) {
-    throw new Error("A faculty member with this email or phone number already exists");
+    throw new Error(
+      "A faculty member with this email or phone number already exists",
+    );
   }
 
   const newFaculty = await db.faculty.create({
