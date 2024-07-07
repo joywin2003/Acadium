0dcf64ed4bfec62c8bdb7a0c13b3671582c4826d
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-07-07 23:02:29 +0530

    feat: added add compose item alert bar

--- src/components/compose-mail.tsx
+++ src/components/compose-mail.tsx
@@ -12,12 +12,16 @@ import {
 import { Button } from "~/components/ui/button";
 import { Input } from "./ui/input";
 import { PenIcon } from "lucide-react";
+import { FormProvider, useForm } from "react-hook-form";
+import { FormControl, FormField, FormItem, FormLabel, Form } from "./ui/form";
+import { Textarea } from "./ui/textarea";
 
 export default function ComposeMail() {
+  const form = useForm();
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
-        <Button  className="rounded-xl px-6 py-6 text-lg space-x-2 absolute bottom-16 right-16 z-10">
+        <Button className="absolute bottom-16 right-16 z-10 space-x-2 rounded-xl px-6 py-6 text-lg">
           <PenIcon className="h-4 w-4 md:h-5 md:w-5" /> <span>Compose</span>
         </Button>
       </AlertDialogTrigger>
@@ -25,13 +29,39 @@ export default function ComposeMail() {
         <AlertDialogHeader>
           <AlertDialogTitle>New Email</AlertDialogTitle>
 
-          <form>
-            <Input id="email" placeholder="Email" />
-          </form>
+          <Form {...form}>
+            <form>
+              <FormField
+                control={form.control}
+                name="subject"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Username" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              ></FormField>
+
+              <FormField
+                control={form.control}
+                name="message"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Message</FormLabel>
+                    <FormControl>
+                      <Textarea placeholder="Type your message here." />
+                    </FormControl>
+                  </FormItem>
+                )}
+              ></FormField>
+            </form>
+          </Form>
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
+          <AlertDialogAction>Send</AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
