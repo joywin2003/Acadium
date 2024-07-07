1c0d663fe6676c5b01b801e724e1db81cf465c45
vyshnav <vyshnavravi599@gmail.com>
2024-07-07 07:53:47 +0530

    refactor: Improve middleware code formatting and add RBAC check for accessing faculyt/new and student/new

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,26 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-export { default } from "next-auth/middleware"
-import { getToken } from "next-auth/jwt"
- 
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+export { default } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
+
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-    const token = await getToken({ req: request })
-    const url = request.nextUrl;
-    if(token && url.pathname === '/') {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
+  if (token && url.pathname === "/") {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
-  if (!token && url.pathname !== '/') {
-    return NextResponse.redirect(new URL('/', request.url));
+  if (!token && url.pathname !== "/") {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  if (url.pathname === "/dashboard/faculty/new" || "/dashboard/student/new") {
+    if (token?.role !== "admin") {
+      return NextResponse.redirect(new URL("/404", request.url));
+    }
   }
 }
- 
+
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/dashboard/:path*', '/'],
-}
+  matcher: ["/dashboard/:path*", "/"],
+};
