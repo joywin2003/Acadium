5917b4e42a0c1ab3586b40b2305a4eb93afcc6af
vyshnav <vyshnavravi599@gmail.com>
2024-07-10 20:15:24 +0530

    feat: Add password hashing for user creation

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -9,14 +9,15 @@ import {
 } from "~/server/api/schema/zod-schema";
 import { db } from "~/server/db";
 import { Faculty, Student, User, Mail } from "~/types";
-import {getServerSession} from "next-auth"
+import { getServerSession } from "next-auth";
 import cuid from "cuid";
 import { sub } from "date-fns";
+import { genSalt, hash } from "bcrypt-ts";
 
 export const getStudentList = async () => {
   try {
     const students: Student[] = await db.student.findMany();
-    console.log("in student")
+    console.log("in student");
     return students;
   } catch (error) {
     console.error("Database connection error: ", error);
@@ -24,8 +25,14 @@ export const getStudentList = async () => {
   }
 };
 
+const hashPassword = async (password: string) => {
+  const salt = await genSalt(10);
+  const hashedPassword = await hash(password, salt);
+  return hashedPassword;
+};
 export const getFacultyList = async () => {
   try {
+    await hashPassword("Hi");
     const faculty: Faculty[] = await db.faculty.findMany();
     return faculty;
   } catch (error) {
@@ -77,6 +84,26 @@ export const createStudent = async (student: TStudentFormSchema) => {
     throw new Error("A student with this email or phone number already exists");
   }
 
+  const isStudentExistInUser = await db.user.findMany({
+    where: {
+      email: student.email,
+    },
+  });
+
+  if (isStudentExistInUser.length > 0) {
+    throw new Error("A student with this email already exists");
+  } else {
+    const hashedPassword = await hashPassword(student.name);
+    const newUser = await db.user.create({
+      data: {
+        email: student.email,
+        name: student.name,
+        role: "student",
+        password: hashedPassword,
+      },
+    });
+  }
+
   const newStudent = await db.student.create({
     data: student,
   });
@@ -105,10 +132,29 @@ export const createFaculty = async (faculty: TFacultyFormSchema) => {
     );
   }
 
-  const newFaculty = await db.faculty.create({
-    data: faculty,
+  const isFacultyExistInUser = await db.user.findMany({
+    where: {
+      email: faculty.email,
+    },
   });
-  return newFaculty;
+
+  if (isFacultyExistInUser.length > 0) {
+    throw new Error("A faculty with this email already exists");
+  } else {
+    const hashedPassword = await hashPassword(faculty.name);
+    const newUser = await db.user.create({
+      data: {
+        email: faculty.email,
+        name: faculty.name,
+        role: "faculty",
+        password: hashedPassword,
+      },
+    });
+    const newFaculty = await db.faculty.create({
+      data: faculty,
+    });
+    return newFaculty;
+  }
 };
 
 export const sendMail = async (mail: TMailSchema) => {
@@ -120,7 +166,7 @@ export const sendMail = async (mail: TMailSchema) => {
     }
     const id = cuid();
     const date = sub(new Date(), { days: 0 }).toISOString();
-  
+
     const newMail: Mail = await db.mail.create({
       data: {
         ...mail,
@@ -129,7 +175,7 @@ export const sendMail = async (mail: TMailSchema) => {
         id,
         read: false,
         date,
-      }
+      },
     });
     return newMail;
   } catch (err) {
