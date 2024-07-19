7314104477bc9c87dc88f3cfb62d0d7e9be6b5ca
vyshnav <vyshnavravi599@gmail.com>
2024-07-19 15:23:58 +0530

    refactor: Update profile page to dynamically render different profiles based on user role

--- src/app/(dashboard)/dashboard/profile/page.tsx
+++ src/app/(dashboard)/dashboard/profile/page.tsx
@@ -1,79 +1,19 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-"use client";;
-import { useQuery } from "@tanstack/react-query";
-import { useSession } from "next-auth/react";
+"use client";
 import React from "react";
-import { getUserProfile } from "~/app/actions";
-import { Button } from "~/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
-import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
-import { type User } from "~/types";
+import StudentProfile from "~/components/profile/studentProfile";
+import { useSession } from "next-auth/react";
+import AdminProfile from "~/components/profile/adminProfile";
+import FacultyProfile from "~/components/profile/facultyProfile";
 
-interface ProfileProps {
-  params: { id: string };
-}
-
-const Profile: React.FC<ProfileProps> = () => {
+const page = () => {
   const { data: session } = useSession();
-  const email = session?.user.email;
-  const { data, error, isLoading } = useQuery<User | null, Error>({
-    queryKey: ["user"],
-    queryFn: async () => await getUserProfile(email as string),
-  });
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error loading user profile</div>;
-  if (!data) return <div>No user profile found</div>;
-
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center">
-      <Card className="h-[550px] w-[550px] text-4xl">
-        <CardHeader>
-          <CardTitle>Profile Information</CardTitle>
-          <CardDescription>Details of the user profile</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="grid w-full items-center gap-4 text-6xl">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" value={data?.name} readOnly />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="email">Email</Label>
-                <Input id="email" value={data?.email} readOnly />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="id">ID</Label>
-                <Input id="id" value={data?.id} readOnly />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="usn">USN</Label>
-                <Input id="usn" value={data?.usn} readOnly />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="branch">Branch</Label>
-                <Input id="branch" value={data?.branch} readOnly />
-              </div>
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button variant="outline">Edit</Button>
-          <Button>Save</Button>
-        </CardFooter>
-      </Card>
+    <div>
+      {session?.user.role === "student" && <StudentProfile />}
+      {session?.user.role === "admin" && <AdminProfile />}
+      {session?.user.role === "faculty" && <FacultyProfile />}
     </div>
   );
 };
 
-export default Profile;
+export default page;
